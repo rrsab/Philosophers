82d162f2cea78c25dc12348d42b02ec718cf1444
salyce <salyce@student.21-school.ru>
2021-08-30 21:52:03 +0300

    one commit

---
+++ philo/includes/philo.h
@@ -0,0 +1,18 @@
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <string.h>
+# include <sys/time.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+typedef struct s_philo
+{
+	int		l_fork;
+	int		r_fork;
+
+};
+
+#endif
